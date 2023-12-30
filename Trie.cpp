00bde983430e132894330635d93a7e306a31afaf
Md Mehedi Hasan <141871742+HuyeHuyeHuye@users.noreply.github.com>
2023-12-30 14:37:22 +0600

    Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -83,70 +83,94 @@ int fy[] = {0, 0, 1, -1, 1 , -1, -1, 1};
 // start...
 struct Node {
 	Node *links[26];
-	bool flag = false;
+	int cntEndWith = 0, cntPrefix = 0;
 	//checks if the reference trie is present or not
-	bool containKey(char ch) {
+	bool containKey(char ch){
 		return (links[ch - 'a'] != NULL);
 	}
 	//creating reference trie
-	void put(char ch, Node *node) {
+	void put(char ch, Node *node){
 		links[ch - 'a'] = node;
 	}
 	//to get the next node for traversal
-	Node *get(char ch) {
+	Node *get(char ch){
 		return links[ch - 'a'];
 	}
-	//setting flag to true at the end of the word
-	void setEnd() {
-		flag = true;
+	
+	void increaseEnd(){
+		cntEndWith++;
 	}
-	//checking if the word is completed or not
-	bool isEnd() {
-		return flag;
+	void increasePrefix(){
+		cntPrefix++;
+	}
+	void deleteEnd(){
+		cntEndWith--;
+	}
+	void reducePrefix(){
+		cntPrefix--;
+	}
+	int getEnd(){
+		return cntEndWith;
+	}
+	int getPrefix(){
+		return cntPrefix;
 	}
 };
-class Trie {
+
+class Trie{
 private:
 	Node* root;
 public:
-	Trie() {
+	Trie(){
 		//creating new obejct
 		root = new Node();
 	}
-
-	void insert(string word) {
-		//initializing dummy node pointing to root initially
-		Node *node = root;
-		for (int i = 0; i < word.size(); i++) {
-			if (!node->containKey(word[i])) {
-				node->put(word[i], new Node());
+	
+	void insert(string word){
+		Node * node = root;
+		for (int i = 0; i < word.length(); i++){
+			if (!node -> containKey(word[i])){
+				node -> put(word[i], new Node());
 			}
-			//moves to reference trie
-			node = node->get(word[i]);
+			node = node -> get(word[i]);
+			node -> increasePrefix();
 		}
-		node->setEnd();
+		node -> increaseEnd();
 	}
-
-	bool search(string word) {
+	
+	int countWordsEqualTo(string &word){
 		Node *node = root;
-		for (int i = 0; i < word.size(); i++) {
-			if (!node->containKey(word[i])) {
-				return false;
-			}
-			node = node->get(word[i]);
-		}
-		return node->isEnd();
-	}
+        for (int i = 0; i < word.length(); i++)
+        {
+            if (node -> containKey(word[i])){
+                node = node -> get(word[i]);
+            }
+            else return 0;
+        }
+        return node -> getEnd();
+    }
 
-	bool startsWith(string prefix) {
-		Node* node = root;
-		for (int i = 0; i < prefix.size(); i++) {
-			if (!node->containKey(prefix[i])) {
-				return false;
-			}
-			node = node->get(prefix[i]);
+	int countWordsStartingWith(string & word){
+		Node * node = root;
+		for (int i = 0; i < word.length(); i++){
+			if(node -> containKey(word[i])){
+				node = node -> get(word[i]);
+			} 
+			else return 0;
 		}
-		return true;
+		return node -> getPrefix();
+	}
+	
+	void erase(string & word){
+		Node * node = root;
+		for (int i = 0; i < word.length(); i++){
+			if (node -> containKey(word[i])){
+				node = node -> get(word[i]);
+				node -> reducePrefix();
+			} 
+			else return;
+		}
+		node -> deleteEnd();
 	}
 };
 // end...
@@ -157,6 +181,7 @@ void solve(){
 	//trie.insert(s);
 	//trie.search(s);
 	//trie.startsWith(s);
+	//trie.erase(s);
 }
 
 int32_t main()
